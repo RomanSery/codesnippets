e89202c38a28f6396dce7c7169ba9ce7ee681d6c
Roman Sery <roman.sery@novelution.com>
2019-07-25 09:36:37 -0400

    Add GenericEnum choiceRenderer

---
+++ src/main/java/org/romansery/util/UIHelpers.java
@@ -0,0 +1,30 @@
+package org.romansery.util;
+
+import java.io.Serializable;
+
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.romansery.enums.GenericEnum;
+
+
+public class UIHelpers implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+		
+	private static final class GenericEnumChoiceRenderer<T extends GenericEnum> extends ChoiceRenderer<T> {
+		private static final long serialVersionUID = 1L;
+        @Override
+		public Object getDisplayValue(T object) {
+        	return object == null ? "" : object.getDescription();
+		}
+        @Override
+		public String getIdValue(T object, int index) {
+			return String.valueOf(object.getId());
+		}
+	}
+
+
+	public static <T extends GenericEnum> IChoiceRenderer<T> getEnumChoiceRenderer() {
+		return new GenericEnumChoiceRenderer<T>();
+	}
+}
