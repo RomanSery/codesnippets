ef43306e6a99d813b3fa8b6ca6df4b515e257707
Roman Sery <roman.sery@novelution.com>
2019-09-13 19:22:40 -0400

    Add Literal component

--- src/main/java/org/coderdreams/webapp/page/PageSizeTest.java
+++ src/main/java/org/coderdreams/webapp/page/PageSizeTest.java
@@ -9,6 +9,7 @@ import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.coderdreams.webapp.BasePage;
 import org.coderdreams.webapp.components.Literal;
@@ -21,6 +22,7 @@ public class PageSizeTest extends BasePage implements IBasePage {
         super();
 
         final boolean useLiteral = params.get("literal") != null && "1".equals(params.get("literal").toString());
+        final boolean useLdm = params.get("ldm") != null && "1".equals(params.get("ldm").toString());
 
         add(new DebugBar("debug"));
 
@@ -29,6 +31,13 @@ public class PageSizeTest extends BasePage implements IBasePage {
             protected void populateItem(ListItem<Integer> item) {
                 if(useLiteral) {
                     item.add(new Literal("num", getRandomString()));
+                } else if(useLdm) {
+                    item.add(new Label("num", new LoadableDetachableModel<String>() {
+                        @Override
+                        protected String load() {
+                            return getRandomString();
+                        }
+                    }));
                 } else {
                     item.add(new Label("num", getRandomString()));
                 }
